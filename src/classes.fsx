380a3354a88b9aab3dd5560d42acf3c41ccfa4bb
KvelaGorrrrnio <Anders@voreswww.dk>
2016-01-11 11:25:42 +0100

    Abstract Animal

--- src/classes.fsx
+++ src/classes.fsx
@@ -1,2 +1,14 @@
+type Position = int*int
+
+/// <summary></summary>
+/// 
 [<AbstractClass>]
-type Animal
+type Animal(breedTime:int,position:Position) =
+  let mutable _position,_age=position,0
+  member this.age with get() = _age
+  member this.position with get() = _position
+  member this.breedTime with get() = breedTime
+  member this.breedClock with get() = 0
+  abstract member move:Position->unit
+  member this.breed() = ()
+  abstract member tick:Array2D->unit
