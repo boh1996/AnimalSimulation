1dbd8e9c74900569fcf369fcb76a14ecb8998817
Bo Hans Thomsen <bht@it-minds.dk>
2016-01-11 13:48:32 +0100

    Started working on Predator

--- src/classes.fsx
+++ src/classes.fsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /// <summary>Stores a Simulation History Record</summary>
 /// <param name="tick:int">The tick time to store this record for</param>
 /// <param name="prey:int">Number of preys</param>
@@ -9,15 +8,11 @@ type HistoryRecord(tick: int, prey: int, predator: int) =
   member prey = prey
   member predator = predator
 
-type Prey(breedTime: int, position: Position)
-  inherit Animal(breedTime, position)
-
-  override this.move(position: Position)
-=======
 type Position = int*int
 
-/// <summary></summary>
-/// 
+/// <summary>Animal</summary>
+/// <param name="breedTime:int">breedTime:int</param>
+/// <param name="position:Position">Current position of the Animal</param>
 [<AbstractClass>]
 type Animal(breedTime:int,position:Position) =
   let mutable _position,_age=position,0
@@ -28,4 +23,17 @@ type Animal(breedTime:int,position:Position) =
   abstract member move:Position->unit
   member this.breed() = ()
   abstract member tick:Array2D->unit
->>>>>>> origin/master
+
+type Prey(breedTime: int, position: Position)
+  inherit Animal(breedTime, position)
+
+  override this.move(position: Position)
+
+type Predator(breedTime: int, starveTime: int, position: Position)
+  inherit Animal(breedTime, position)
+  let mutable _starveTime = starveTime
+  member this.starveTime with get() = _starveTime
+  member this.starveClock with get() = 0
+
+  override this.move(position: Position)
+  member this.eat() = ()
