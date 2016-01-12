69ab93c3f78fd591e504a709fa25743344f524c0
KvelaGorrrrnio <Anders@voreswww.dk>
2016-01-12 16:00:51 +0100

    design and stuff

--- src/classes.fsx
+++ src/classes.fsx
@@ -178,9 +178,14 @@ and Simulation(settings:Settings) =
       let h = new HistoryRecord(this.clockTick, prey, pred)
       this.history <- Array.append this.history [||]
       json <- json + "\t" + h.toJSON()
-      if i < this.settings.timeSpan then json <- json + ",\n"
+      if i < this.settings.timeSpan-1 then json <- json + ",\n"
     json <- sprintf "[\n%s\n]" json
-    System.IO.File.WriteAllText("./output/" + (DateTime.Now.ToString()) + ".json",json)
+    let name = DateTime.Now.ToString()
+    System.IO.File.WriteAllText("./output/" + name  + ".json",json)
+    System.Console.Clear()
+    printfn "History records has been saved in \"output/%s.json\"!" name
+    System.Console.ReadKey() |> ignore
+    System.Console.Clear()
 
   member this.kill (x, y) =
     this.grid.[x, y].Value.isDead <- true
@@ -193,6 +198,5 @@ and Simulation(settings:Settings) =
 
   member this.addPredator (x, y) =
     let animal = Predator(this, x, y, this.settings.predatorBreedTime, this.settings.starveTime)
-    printfn "%O" (x,y)
     this.animals <- (upcast animal)::this.animals
     this.grid.[x, y] <- Some(upcast animal)
