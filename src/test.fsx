68408433038c16f3a8b0a676949a2fef67bba923
Mads Ulrik Svendsen <mail@madsulrik.com>
2016-01-12 18:21:24 +0100

    test fixed

--- src/test.fsx
+++ src/test.fsx
@@ -32,7 +32,7 @@ let Assert f b str =
 // 
 // Settings tests
 //
-let filePath = "/settings/default.json"
+let filePath = "settings/default.json"
 let s = new Settings(filePath)
 
 let settingsTest1 = Assert (fun () -> s.width) 12 "Settings load json test 01"
@@ -40,23 +40,25 @@ let settingsTest1 = Assert (fun () -> s.width) 12 "Settings load json test 01"
 // 
 // Simulate tests
 //
-let simulation = new Simulation(settings)
+let simulation = new Simulation(s)
+
+let grid = simulation.grid
 
 // Add prey
 simulation.addPrey (2,2)
-let t2 = Assert (fun () -> simulation.grid[2, 2] <> None) true "Add prey test 01"
+let t2 = Assert (fun () -> simulation.grid.[2, 2] <> None) true "Add prey test 01"
 
 // Add predator
 simulation.addPredator (2,3)
-let t3 = Assert (fun () -> simulation.grid[2, 3] <> None) true "Add predator test 01"
+let t3 = Assert (fun () -> simulation.grid.[2, 3] <> None) true "Add predator test 01"
 
-// Find adjacent preys
-let adj = simulation.animals[1].adjacentPrey
-let t4 = Assert (fun () -> adj.length) 1 "Adjacent prey test 01"
+// Find adjacent animals
+let adj = simulation.animals.[0].adjacentFilled
+let t4 = Assert (fun () -> adj.IsSome) true "Adjacent prey test 01"
 
 // Kill an Animal
 simulation.kill (2, 2)
-let t5 = Assert (fun () -> simulation.grid[2, 2] = None) true "Kill test 01"
+let t5 = Assert (fun () -> simulation.grid.[2, 2] = None) true "Kill test 01"
 
 let tests = [
     settingsTest1;
